dc4a44daaa52a6c561f1619853fec9d7817ad834
Duynghiagh <134992743+Duynghiagh@users.noreply.github.com>
2023-12-04 11:20:47 +0700

    sadas

---
+++ DAL1/DomainClass/DaoDien.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL1.DomainClass;
+
+[Table("DaoDien")]
+public partial class DaoDien
+{
+    [Key]
+    [StringLength(10)]
+    public string MaDaoDien { get; set; } = null!;
+
+    [StringLength(30)]
+    public string TenDaoDien { get; set; } = null!;
+
+    public string? MoTaDaoDien { get; set; }
+
+    [InverseProperty("MaDaoDienNavigation")]
+    public virtual ICollection<Phim> Phims { get; set; } = new List<Phim>();
+}
---
+++ DAL1/DomainClass/DienVien.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL1.DomainClass;
+
+[Table("DienVien")]
+public partial class DienVien
+{
+    [Key]
+    [StringLength(10)]
+    public string MaDienVien { get; set; } = null!;
+
+    [StringLength(30)]
+    public string TenDienVien { get; set; } = null!;
+
+    public string? MoTaDienVien { get; set; }
+
+    [InverseProperty("MaDienVienNavigation")]
+    public virtual ICollection<Phim> Phims { get; set; } = new List<Phim>();
+}
