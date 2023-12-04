b287eda9998ddc11cbb808c91e29de4be337ab84
dungdhph40083 <147228552+dungdhph40083@users.noreply.github.com>
2023-12-04 11:21:44 +0700

    test push, also fixing one single piece of user interface for no reason

--- PRL/DichVu.Designer.cs
+++ PRL/DichVu.Designer.cs
@@ -68,33 +68,34 @@
             panel_DichVu.Controls.Add(label3);
             panel_DichVu.Controls.Add(pictureBox1);
             panel_DichVu.Controls.Add(dataGridView1);
-            panel_DichVu.Location = new Point(3, 16);
-            panel_DichVu.Margin = new Padding(4, 3, 4, 3);
+            panel_DichVu.Location = new Point(2, 10);
+            panel_DichVu.Margin = new Padding(3, 2, 3, 2);
             panel_DichVu.Name = "panel_DichVu";
-            panel_DichVu.Size = new Size(1154, 1175);
+            panel_DichVu.Size = new Size(808, 752);
             panel_DichVu.TabIndex = 0;
             // 
             // comboBox1
             // 
             comboBox1.BackColor = Color.FromArgb(255, 138, 0);
-            comboBox1.FlatStyle = FlatStyle.Flat;
+            comboBox1.FlatStyle = FlatStyle.Popup;
             comboBox1.ForeColor = Color.Black;
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(414, 547);
-            comboBox1.Margin = new Padding(4, 3, 4, 3);
+            comboBox1.Location = new Point(290, 350);
+            comboBox1.Margin = new Padding(3, 2, 3, 2);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(351, 33);
+            comboBox1.Size = new Size(247, 24);
             comboBox1.TabIndex = 89;
             // 
             // button3
             // 
             button3.BackColor = Color.FromArgb(112, 88, 61);
+            button3.FlatStyle = FlatStyle.System;
             button3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             button3.ForeColor = Color.White;
-            button3.Location = new Point(795, 530);
-            button3.Margin = new Padding(4, 3, 4, 3);
+            button3.Location = new Point(556, 339);
+            button3.Margin = new Padding(3, 2, 3, 2);
             button3.Name = "button3";
-            button3.Size = new Size(217, 53);
+            button3.Size = new Size(152, 34);
             button3.TabIndex = 87;
             button3.Text = "Tạo Dịch Vụ";
             button3.UseVisualStyleBackColor = false;
@@ -105,21 +106,22 @@
             textBox2.BorderStyle = BorderStyle.None;
             textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             textBox2.ForeColor = Color.Black;
-            textBox2.Location = new Point(416, 678);
-            textBox2.Margin = new Padding(4, 3, 4, 3);
+            textBox2.Location = new Point(294, 434);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(353, 32);
+            textBox2.Size = new Size(239, 22);
             textBox2.TabIndex = 86;
             // 
             // button4
             // 
             button4.BackColor = Color.FromArgb(112, 88, 61);
+            button4.FlatStyle = FlatStyle.System;
             button4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             button4.ForeColor = Color.White;
-            button4.Location = new Point(795, 614);
-            button4.Margin = new Padding(4, 3, 4, 3);
+            button4.Location = new Point(556, 393);
+            button4.Margin = new Padding(3, 2, 3, 2);
             button4.Name = "button4";
-            button4.Size = new Size(217, 53);
+            button4.Size = new Size(152, 34);
             button4.TabIndex = 88;
             button4.Text = "Cập Nhật";
             button4.UseVisualStyleBackColor = false;
@@ -130,10 +132,10 @@
             textBox3.BorderStyle = BorderStyle.None;
             textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             textBox3.ForeColor = Color.Black;
-            textBox3.Location = new Point(415, 617);
-            textBox3.Margin = new Padding(4, 3, 4, 3);
+            textBox3.Location = new Point(294, 395);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(353, 32);
+            textBox3.Size = new Size(239, 22);
             textBox3.TabIndex = 87;
             // 
             // label2
@@ -141,10 +143,9 @@
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(184, 672);
-            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Location = new Point(129, 430);
             label2.Name = "label2";
-            label2.Size = new Size(222, 38);
+            label2.Size = new Size(151, 25);
             label2.TabIndex = 56;
             label2.Text = "Chi Tiết Dịch Vụ:";
             // 
@@ -154,10 +155,10 @@
             textBox5.BorderStyle = BorderStyle.None;
             textBox5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             textBox5.ForeColor = Color.Black;
-            textBox5.Location = new Point(415, 487);
-            textBox5.Margin = new Padding(4, 3, 4, 3);
+            textBox5.Location = new Point(294, 312);
+            textBox5.Margin = new Padding(3, 2, 3, 2);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(353, 32);
+            textBox5.Size = new Size(239, 22);
             textBox5.TabIndex = 88;
             // 
             // label1
@@ -165,10 +166,9 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(184, 611);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(129, 391);
             label1.Name = "label1";
-            label1.Size = new Size(168, 38);
+            label1.Size = new Size(115, 25);
             label1.TabIndex = 57;
             label1.Text = "Giá Dịch Vụ:";
             // 
@@ -177,20 +177,19 @@
             label13.AutoSize = true;
             label13.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
             label13.ForeColor = Color.White;
-            label13.Location = new Point(184, 545);
-            label13.Margin = new Padding(4, 0, 4, 0);
+            label13.Location = new Point(129, 349);
             label13.Name = "label13";
-            label13.Size = new Size(206, 38);
+            label13.Size = new Size(140, 25);
             label13.TabIndex = 59;
             label13.Text = "Mô Tả Dịch Vụ:";
             // 
             // pictureBox4
             // 
             pictureBox4.Image = Properties.Resources.Rectangle_481;
-            pictureBox4.Location = new Point(414, 484);
-            pictureBox4.Margin = new Padding(4, 3, 4, 3);
+            pictureBox4.Location = new Point(290, 310);
+            pictureBox4.Margin = new Padding(3, 2, 3, 2);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(353, 41);
+            pictureBox4.Size = new Size(247, 26);
             pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox4.TabIndex = 93;
             pictureBox4.TabStop = false;
@@ -200,20 +199,19 @@
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
             label7.ForeColor = Color.White;
-            label7.Location = new Point(184, 481);
-            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Location = new Point(129, 308);
             label7.Name = "label7";
-            label7.Size = new Size(167, 38);
+            label7.Size = new Size(114, 25);
             label7.TabIndex = 60;
             label7.Text = "Mã Dịch Vụ:";
             // 
             // pictureBox3
             // 
             pictureBox3.Image = Properties.Resources.Rectangle_481;
-            pictureBox3.Location = new Point(414, 614);
-            pictureBox3.Margin = new Padding(4, 3, 4, 3);
+            pictureBox3.Location = new Point(290, 393);
+            pictureBox3.Margin = new Padding(3, 2, 3, 2);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(353, 41);
+            pictureBox3.Size = new Size(247, 26);
             pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox3.TabIndex = 92;
             pictureBox3.TabStop = false;
@@ -223,20 +221,19 @@
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
             label3.ForeColor = Color.White;
-            label3.Location = new Point(4, 34);
-            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Location = new Point(3, 22);
             label3.Name = "label3";
-            label3.Size = new Size(114, 38);
+            label3.Size = new Size(78, 25);
             label3.TabIndex = 51;
             label3.Text = "Dịch Vụ";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.Rectangle_481;
-            pictureBox1.Location = new Point(415, 675);
-            pictureBox1.Margin = new Padding(4, 3, 4, 3);
+            pictureBox1.Location = new Point(290, 432);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(353, 41);
+            pictureBox1.Size = new Size(247, 26);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 91;
             pictureBox1.TabStop = false;
@@ -245,22 +242,22 @@
             // 
             dataGridView1.BackgroundColor = Color.White;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(0, 102);
-            dataGridView1.Margin = new Padding(4, 3, 4, 3);
+            dataGridView1.Location = new Point(0, 65);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
             dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.Size = new Size(1154, 334);
+            dataGridView1.Size = new Size(808, 214);
             dataGridView1.TabIndex = 0;
             // 
             // DichVu
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 16F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(190, 0, 0);
-            ClientSize = new Size(1171, 892);
+            ClientSize = new Size(820, 571);
             Controls.Add(panel_DichVu);
-            Margin = new Padding(4, 3, 4, 3);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "DichVu";
             Text = "DichVu";
             panel_DichVu.ResumeLayout(false);
