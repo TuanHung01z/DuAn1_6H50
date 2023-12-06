56d6081178051d49797a1eaae17d2b17fb9a7ff5
Sir_Giap <140328785+mrgiap736@users.noreply.github.com>
2023-12-06 09:55:30 +0700

    Hùng đã code chức năng khách hàngn

---
+++ BUS/IServices/IKhachHangServices.cs
@@ -0,0 +1,17 @@
+﻿using DAL1.DomainClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS.IServices
+{
+    internal interface IKhachHangServices
+    {
+        public string Add(KhachHang kh);
+        public string Update(KhachHang kh);
+        public string Remove(KhachHang kh);
+        List<KhachHang> GetAll(string search);
+    }
+}
---
+++ DAL1/IRepos/IKhachHangRepo.cs
@@ -0,0 +1,18 @@
+﻿using DAL1.DomainClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL1.IRepos
+{
+    public interface IKhachHangRepo
+    {
+        public bool AddKH(KhachHang kh);
+        public bool UpdateKH(KhachHang kh);
+        public bool RemoveKH(KhachHang kh);
+        public List<KhachHang> GetAllKH();
+
+    }
+}
